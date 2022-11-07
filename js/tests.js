387b7a4a85b8305f6e606df4cd257f81b321dd2e
Freddy Vegas <vegasfalexis@gmail.com>
2022-11-07 13:01:26 +0100

    cuatro exercises ready

--- js/tests.js
+++ js/tests.js
@@ -1,88 +1,181 @@
-const chai = window.chai
-const expect = chai.expect
+const newLocal = window.chai;
+const chai = newLocal;
+const expect = chai.expect;
 
-describe('Las variables en Javascript', () => {
-  it('pueden contener strings', () => {
-    // Añade la línea que falta
-    const a = "Javascript"
-    expect(a).to.equal("Javascript");
-  })
-  it('pueden contener números', () => {
-    // Añade la línea que falta
+describe("Variables en JS", () => {
+    it("Completa todas las constantes que faltan", () => {
 
-    expect(b).to.equal(14.64);
-  })
-  it('pueden contener booleanos', () => {
-    // Añade la línea que falta
-
-    expect(c).to.equal(false);
-  })
-})
-
-describe('Comparaciones en Javascript', () => {
-    it("usando ===", () => {
-        const number = 20;
-        //completa la asignación usando number y el comparador ===
-
-        expect(expresion).to.equal(false)
-    })
-
-    it("usando !==", () => {
-        const language = "Javascript";
-        //completa la asignación usando language y el comparador !==
-
-        expect(expresion).to.equal(false)
-    })
-})
-
-describe("Operaciones aritméticas", () => {
-    it("Usando suma", () => {
         const a = 10;
+        const b = 1.34;
+        const c = true;      
+        const d = "Buenos días"
+        const e = "Pepe"
+        const f = "Buenos días, pepe"
 
-        expect(a+b).to.equal(100);
+        expect(a).to.equal(10);
+        expect(b).to.equal(1.34);
+        expect(c).to.equal(true);
+        expect(d).to.equal("Buenos días");
+        expect(e).to.equal("Pepe");
+        expect(f).to.equal("Buenos días, pepe");
     })
-    it("Usando multiplicación", () => {
-        const a = 10;
 
-        expect(a*b).to.equal(13);
+    it("Completa todas las variables que faltan para que las operaciones resulten correctamente", () => {
+
+        let a = 11;
+        let b = 0.34;
+        let c = 10;
+        let d = 100;
+        let e =  440;
+        let f = 10
+        let g = 3
+        let x = 2
+        let y = 1;
+    
+
+
+        expect(a + b).to.equal(11.34);
+        expect(a * c).to.equal(110);
+        expect(d - e).to.equal(-340);
+        // investiga para qué sirven los operadores ** y %
+        expect(f ** g).to.equal(1000);
+        expect(x % 2).to.equal(0);
+        expect(y % 2).to.equal(1);
     })
-    it("Usando división", () => {
-        const a = 10;
 
-        expect(a/b).to.equal(2.5);
+    it("Completa todas las variables que faltan para que se cumplan las condiciones", () => {
+
+        let a = 10;
+        let b = 100
+        let c = undefined
+        let d = "Hell"
+        let n = "b"
+
+        expect(a > 9).to.be.true;
+        expect(a < 11).to.be.true;
+        expect(b === 100).to.be.true;
+        expect(c === undefined).to.be.true;
+        expect(d !== "Hello").to.be.true;
+        expect(n.startsWith('A')).to.be.false;
     })
 })
 
-describe('Algebra booleana', () => {
-    it("Usando el operador AND (&&)", () => {
-        const a = true;
-        //completa la asignación de b
+describe("condicionales en javascript", () => {
+    it("completa el valor del resultado esperado (expected)", () => {
 
-        const expression = a && b;
-        expect(expression).to.equal(true);
+        let name = "Pepe";
+
+        let result = "KO";
+
+        // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/length
+        if (name.length === 6) {
+            result = "OK"
+        }
+            
+        // substituye ??? por el valor que tiene la variable "result"        
+        expect(result === "KO").to.be.true;
     })
-    it("Usando el operador OR (||)", () => {
-        const a = false;
-        //completa la asignación de b
 
-        const expression = a || b;
-        expect(expression).to.equal(false);
+    it("comparando cadenas (strings)", () => {
+
+        let a = 'a';
+        let b = 'b';
+        let result;
+
+        if (a < b) {
+            result = 'pikachu';
+        } else {
+            result = 'charmander';
+        }
+
+        // substituye ??? por el valor que tiene la variable "result"        
+        expect(result === "???").to.be.true;
+    })
+
+    it("Interpolación de cadenas (strings) (1)", () => {
+        let n = "Piña";
+        let m = "Pepperoni";
+        let q = 14;
+        let result;
+
+        // https://developer.mozilla.org/es/docs/Learn/JavaScript/First_steps/Math#operadores_aritm%C3%A9ticos
+        if (q % 5 === 0) {
+            result = "Pizza con " + n;
+        } else {
+            result = "Pizza con " + m;
+        }
+
+        // substituye ??? por el valor que tiene la variable "result"        
+        expect(result === "???").to.be.true;
+    })
+
+    it("Interpolación de cadenas (strings) (2)", () => {
+        let n = "Piña";
+        let m = "Pepperoni";
+        let q = 14;
+        let result;
+
+        // https://developer.mozilla.org/es/docs/Learn/JavaScript/First_steps/Math#operadores_aritm%C3%A9ticos
+        if (q % 7 === 0) {
+            result = `Pizza con ${n}`;
+        } else {
+            result = `Pizza con ${m}`;
+        }
+
+        // substituye ??? por el valor que tiene la variable "result"        
+        expect(result === "???").to.be.true;
     })
 })
 
+describe("Funciones en JS", () => {
 
-// Para hacer pasar estos tests, edita el código correspondiente
-// en el archivo "code.js"
-describe("Ejemplos de funciones", () => {
-    it("Función que nos dice si un número es par (even) o impar (odd)", () => {
-        expect(evenOrOdd(2)).to.equal("even")
-        expect(evenOrOdd(13)).to.equal("odd")
+    function f(a, b, c) {
+        return b - a + 4 * c;
+    }
+
+    function g(n, m) {
+        return `Son ${m} y ${n}`;
+    }
+
+    function h(name) {
+        
+        // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/length
+        if (name.length <= 4) {
+            return "too short";
+        } else if (name.length < 7) {
+            return "adecquate";
+        } else if (name.length < 12) {
+            return "long";
+        } else {
+            return "very long";
+        }
+    }
+
+    it("Cuál es el resultado de invocar la función? (1)", () => {
+
+        let result = f(10, 30, 2);
+
+
+        // substituye "???" por el valor que tiene la variable "result"        
+        expect(result === "???").to.be.true;
     })
 
-    it ("Función que nos devuelve un saludo para cada nombre", () => {
-        expect(greeting("Javi")).to.equal("Hola, Javi!")
-        expect(greeting("gente")).to.equal("Hola, gente!")
-        expect(greeting("Mary")).to.equal("Hola, Mary!")
+    it("Cuál es el resultado de invocar la función? (2)", () => {
+
+        let result = g("Cerebro", "Pinky");
+        
+
+        // substituye "???" por el valor que tiene la variable "result"        
+        expect(result === "???").to.be.true;
     })
+
+    it("Cuál es el resultado de invocar la función? (3)", () => {
+
+        // substituye "???" por el resultado en cada caso
+        expect(h("khaleesi mother of dragons breaker of chains") === "???").to.be.true;
+        expect(h("sarah") === "???").to.be.true;
+        expect(h("bob") === "???").to.be.true;
+        expect(h("robertson") === "???").to.be.true;
+    })
+
 })
-
